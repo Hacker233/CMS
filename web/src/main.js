24f0c2c22c895930654b180c9a6f0ce0db193ed0
hacker233 <lhqhacker@163.com>
2021-07-04 18:57:08 +0800

    评论完善

--- web/src/main.js
+++ web/src/main.js
@@ -13,8 +13,11 @@ import IconPig from "./components/IconSvg/IconPig.vue";
 import moment from "moment"; // 日期处理插件
 import MessageTree from "message-tree"; // 评论列表组件树
 import CommentEditor from "comment-message-editor"; // 评论组件编辑框
+import Comments from 'vue-comments'
 
+Vue.use(Comments)
 Vue.use(MessageTree);
+
 
 Vue.prototype.$moment = moment;
 Vue.component("IconPig", IconPig);
--- web/src/views/articleDetails/index.vue
+++ web/src/views/articleDetails/index.vue
@@ -8,7 +8,7 @@
           :articleItem="articleItem"
         ></article-content>
         <!-- 评论组件 -->
-        <comment-com :commentList="commentList" @submit="submit"></comment-com>
+        <comment-index :commentList="commentList"></comment-index>
       </article>
       <div class="article-right">
         <!-- 右侧用户详情区域 -->
@@ -20,12 +20,11 @@
   </div>
 </template>
 <script>
-import CommentCom from "./components/Comment.vue"; // 评论组件
+import CommentIndex from "./components/CommentIndex.vue"; // 评论组件
 import ArticleContent from "./components/ArticleContent"; // 文章详情
 import UserBox from "@/components/UserBox/UserBox"; // 用户卡片
 import HandleBox from "./components/HandleBox.vue"; // 点赞等操作浮窗
 import { getArticleInfo } from "@/service/api/article.js";
-import { getCommentList, addComment } from "@/service/api/comment.js";
 export default {
   data() {
     return {
@@ -36,12 +35,11 @@ export default {
   components: {
     UserBox,
     ArticleContent,
-    CommentCom,
+    CommentIndex,
     HandleBox,
   },
   mounted() {
     this.articleInfo(); // 获取文章信息
-    this.getCommentList(); // 获取评论列表
   },
   methods: {
     // 获取文章详情
@@ -55,45 +53,6 @@ export default {
       } else {
         this.$message.error(data.message);
       }
-    },
-    // 获取评论列表
-    async getCommentList() {
-      let params = {
-        article_id: this.$route.query.id,
-      };
-      const data = await getCommentList(params);
-      if (data.code === "00000") {
-        if (data.data) {
-          this.commentList = data.data;
-        } else {
-          this.commentList = [];
-        }
-      } else {
-        this.$message.error(data.message);
-      }
-    },
-    // 提交一级评论
-    async submit(content) {
-      let params = {
-        article_id: this.$route.query.id,
-        content: content,
-      };
-      const data = await addComment(params);
-      if (data.code === "00000") {
-        this.$message.success("评论成功");
-        this.getCommentList();
-      } else {
-        this.$message.error(data.message);
-      }
-    },
-    doSend(content) {
-      console.log("初始发送按钮点击事件：" + content);
-    },
-    doChidSend(args) {
-      console.log("评论区发送按钮点击事件：");
-      console.log("content=" + args[0]);
-      console.log("targetUserId=" + args[1]);
-      console.log("父级评论id=" + args[2]);
     },
   },
 };
